ae43c8416cff5d5c3321bbe6da0cb6121bc2c02d
LeonardoVFF <leonardovff@gmail.com>
2016-11-21 23:30:27 -0300

    adição de menu monile e desktop

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+var get = {
+    item: function(item, dom){
+        return typeof(dom)!="undefined"?dom.querySelector(item):document.querySelector(item);
+    },
+    all: function(itens, dom){
+        return typeof(dom)!="undefined"?dom.querySelectorAll(itens):document.querySelectorAll(itens);
+    }
+},
+app = {
+	setEvents : function(){
+		get.item('.btn-menu').addEventListener('click', function(el){ 
+			get.item(".menu-nav").className = get.item(".menu-nav").className.indexOf("mobile-active")!= -1?
+			"menu-nav":"menu-nav mobile-active";			
+		}, false);
+	},
+	init: function(){
+		app.setEvents();
+	}
+}
+window.onload = app.init();

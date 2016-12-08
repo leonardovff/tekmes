9bc57f0a1cd20b1c6ff4300d40c5f85168c60b0c
LeonardoVFF <leonardovff@gmail.com>
2016-12-08 05:55:42 -0300

    Adição da area de noticias

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,16 +7,29 @@ var get = {
     }
 },
 app = {
+	pageResize: function(){
+		console.log("entrou");
+		var lnItem = $(".ln-item"),
+		lnItemWidth = $(lnItem[0]).outerWidth();
+		lnItem.each(function(i){
+			var perc = $(this).hasClass('ln-bigger')?1.2:0.6;
+			$(this).css('height',lnItemWidth*perc+"px");
+		});
+	},
 	setEvents : function(){
 		get.item('.btn-menu').addEventListener('click', function(el){ 
 			get.item(".menu-nav").className = get.item(".menu-nav").className.indexOf("mobile-active")!= -1?
 			"menu-nav":"menu-nav mobile-active";			
 		}, false);
+		$(window).on("debouncedresize", function( event ) {
+			app.pageResize();	
+		});
 	},
 	init: function(){
 		var carousel;
 	    carousel = $("#scrolling>ul");
 	    carousel.itemslide();
+	    app.pageResize();
 		app.setEvents();
 	}
 }

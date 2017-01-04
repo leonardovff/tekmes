7b8f2456908ff6d75c939f5df14b21891240cc02
leonardovff <leonardovff@gmail.com>
2017-01-04 17:17:16 -0300

    Adição de menu fixo a depender da resolução

--- assets/js/servicos.js
+++ assets/js/servicos.js
@@ -1,11 +1,37 @@
 (function(){ 
+var navbar = {
+    el: $('.headerInside-navbar'),
+    height: 0,
+    captureHeight: function(){
+        navbar.height = navbar.el.outerHeight();
+    },
+    changeStatus: function(){
+        if($(window).scrollTop() >= $('.headerInside').outerHeight()-navbar.height) {
+            return navbar.el.addClass('headerInside-navbar--show');
+        }
+        if(navbar.el.hasClass('headerInside-navbar--show')){
+            navbar.el.removeClass('headerInside-navbar--show');
+        }
+    }
+}
 var app = {
 	pageResize: function(){
+        navbar.captureHeight();
+        
 		$('.view-servico').css("paddingTop", ($(".headerInside").outerHeight()-1)+"px");
 		$('.view-service').css("paddingBottom", ($(".footerContact").outerHeight()-1)+"px");
     },
 	setEvents : function(){
+		$(window).scroll($.throttle(100, function(){
+			app.scrolled(); 
+		}));
+		$(window).on("debouncedresize", function( event ) {
+			app.pageResize();	
+		});
 	},
+    scrolled: function(){
+        navbar.changeStatus();
+    },
 	init: function(){
 		setTimeout(function(){
 	    	app.pageResize();

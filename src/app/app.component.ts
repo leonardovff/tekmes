6ff7fe8d831ca78e3d1b4342d18c1a7a57e42171
LeonardoVFF <leonardovff@gmail.com>
2017-02-26 20:36:22 -0300

    Suport ao jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  ngAfterViewInit(){
+    var x = $('p');
+    console.log(x);
+  }
 }

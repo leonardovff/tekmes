a16d95629ce50ecee4263f1b47a0a1dbd536165d
LeonardoVFF <leonardovff@gmail.com>
2017-02-27 14:23:52 -0300

    Adição do window.service

---
+++ src/app/service/window.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { WindowService } from './window.service';
+
+describe('WindowServiceService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [WindowService]
+    });
+  });
+
+  it('should ...', inject([WindowService], (service: WindowService) => {
+    expect(service).toBeTruthy();
+  }));
+});
---
+++ src/app/service/window.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+
+@Injectable()
+export class WindowService {
+  width$: Observable<number>;
+    //create more Observables as and when needed for various properties
+  hello: string = "Hello";
+  constructor() {
+      let windowSize$ = new BehaviorSubject(getWindowSize());
+
+      this.width$ = (windowSize$.pluck('width') as Observable<number>).distinctUntilChanged();
+
+      Observable.fromEvent(window, 'resize')
+          .debounceTime(100)
+          .map(getWindowSize)
+          .subscribe(windowSize$);
+
+  }
+
+}
+function getWindowSize() {
+    return {
+        width: window.innerWidth
+        //you can sense other parameters here
+    };
+};

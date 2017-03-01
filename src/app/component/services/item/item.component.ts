f662e388ee84ed4ad5f7d0eda7400f8b64516e74
LeonardoVFF <leonardovff@gmail.com>
2017-03-01 01:31:54 -0300

    Services como quadrado

--- src/app/component/services/item/item.component.ts
+++ src/app/component/services/item/item.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, ViewChild, ElementRef } from '@angular/core';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
+
+
+import { WindowService } from '../../../service/window.service';
+declare var $:any;
 
 @Component({
   selector: 'service-list',
@@ -7,11 +11,41 @@ import {AngularFire, FirebaseListObservable} from 'angularfire2';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+  @ViewChild('list') listServices:ElementRef;
+
   services: FirebaseListObservable<any[]>;
-  constructor(af: AngularFire) {
+  width;
+
+  constructor(af: AngularFire,private windowService:WindowService) {
+    windowService.width$.subscribe((value:any) => {
+        setTimeout(() => {
+          this.onResize();
+        },200)
+    });
     this.services = af.database.list('/servicos');
+    this.services.subscribe((e) => {
+      setTimeout(() => {
+        this.onResize();
+      },200);
+    });
   }
   ngOnInit() {
   }
+  onResize(){
+      console.log("Entrou");
+    if(typeof(this.listServices)=="undefined") return false;
+    let lnItem = $(this.listServices.nativeElement).find('li');
+    if(lnItem.length == 0) return false;
+    let el = $('.menu-nav>ul');
 
+    let lnItemWidth = $(lnItem[0]).outerWidth();
+    let perc = (lnItemWidth*1)+"px";
+    if(el.css('display')=="none" ||
+      el.hasClass('mobile-active') && el.css('position') == "absolute"){
+        perc = 'auto';
+    }
+    lnItem.each(function(i){
+      $(this).css('height',perc);
+    });
+  }
 }
